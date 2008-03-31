2d150916ff36a0954b33eb16e64eb3648d11dc22
Matthew Gabeler-Lee <cheetah@fastcat.org>
2008-03-31 22:44:21 +0000

    make the test suite work on freebsd

--- tarix/test/04-index.sh
+++ tarix/test/04-index.sh
@@ -10,17 +10,22 @@ cp bin/test/data bin/test/data.d/data2
 
 sources="data data.d"
 
-tar -C bin/test/ -c $sources | bin/tarix -zf bin/test/data.tarix >bin/test/data.tgz
+tar -c -f - -C bin/test/ $sources | bin/tarix -zf bin/test/data.tarix >bin/test/data.tgz
 [ -f bin/test/data.tarix ] || exit 1
 [ -f bin/test/data.tgz ] || exit 1
 cat bin/test/data.tarix
 #zcat bin/test/data.tgz | hexdump -C
-tar -tzvf bin/test/data.tgz
+tar -tzv -f bin/test/data.tgz
 
-tar -C bin/test/ -c -V 'test backup of data, data.d/data*' --listed-incremental=bin/test/data.list $sources | bin/tarix -zf bin/test/data.incr.tarix >bin/test/data.incr.tgz
-[ -f bin/test/data.list ] || exit 1
-[ -f bin/test/data.incr.tarix ] || exit 1
-[ -f bin/test/data.incr.tgz ] || exit 1
-cat bin/test/data.incr.tarix
-#zcat bin/test/data.incr.tgz | hexdump -C
-tar -tzvf bin/test/data.incr.tgz
+# bsd tar doesn't support incremental or volume label
+if tar --version | grep GNU.tar ; then
+  tar -c -f - -C bin/test/ -V 'test backup of data, data.d/data*' \
+    --listed-incremental=bin/test/data.list $sources \
+    | bin/tarix -zf bin/test/data.incr.tarix >bin/test/data.incr.tgz
+  [ -f bin/test/data.list ] || exit 1
+  [ -f bin/test/data.incr.tarix ] || exit 1
+  [ -f bin/test/data.incr.tgz ] || exit 1
+  cat bin/test/data.incr.tarix
+  #zcat bin/test/data.incr.tgz | hexdump -C
+  tar -tzv -f bin/test/data.incr.tgz
+fi
--- tarix/test/05-extract.sh
+++ tarix/test/05-extract.sh
@@ -4,13 +4,16 @@ set -xe
 
 [ -f bin/test/data.tgz ] || exit 1
 [ -f bin/test/data.tarix ] || exit 1
-[ -f bin/test/data.incr.tgz ] || exit 1
-[ -f bin/test/data.incr.tarix ] || exit 1
 
 bin/tarix -zxf bin/test/data.tarix -t bin/test/data.tgz data.d/data1 >bin/test/data.x.tar
 #hexdump -C bin/test/data.x.tar
 tar -tvf bin/test/data.x.tar
 
-bin/tarix -zxf bin/test/data.incr.tarix -t bin/test/data.incr.tgz data.d/data1 >bin/test/data.incr.x.tar
-#hexdump -C bin/test/data.incr.x.tar
-tar -tvf bin/test/data.incr.x.tar
+# bsd tar doesn't support incremental or volume label
+if tar --version | grep GNU.tar ; then
+  [ -f bin/test/data.incr.tgz ] || exit 1
+  [ -f bin/test/data.incr.tarix ] || exit 1
+  bin/tarix -zxf bin/test/data.incr.tarix -t bin/test/data.incr.tgz data.d/data1 >bin/test/data.incr.x.tar
+  #hexdump -C bin/test/data.incr.x.tar
+  tar -tvf bin/test/data.incr.x.tar
+fi
