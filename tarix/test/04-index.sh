bc8d1e352fc27a3700466d78d02653992ebc8366
Matthew Gabeler-Lee <cheetah@fastcat.org>
2010-05-26 14:38:43 +0000

    work around bug in gnu tar 1.22.x where incremental list is not interpreted w.r.t. -C option

--- tarix/test/04-index.sh
+++ tarix/test/04-index.sh
@@ -19,8 +19,10 @@ tar -tzv -f bin/test/data.tgz
 
 # bsd tar doesn't support incremental or volume label
 if tar --version | grep GNU.tar ; then
-  tar -c -f - -C bin/test/ -V 'test backup of data, data.d/data*' \
-    --listed-incremental=bin/test/data.list $sources \
+  # old versions (1.22.x at least) of tar have a bug handling -C properly
+  # so instead we do the cd ourselves to avoid trying to detect that
+  ( cd bin/test && tar -c -f - -V 'test backup of data, data.d/data*' \
+    --listed-incremental=data.list $sources ) \
     | bin/tarix -zf bin/test/data.incr.tarix >bin/test/data.incr.tgz
   [ -f bin/test/data.list ]
   [ -f bin/test/data.incr.tarix ]
