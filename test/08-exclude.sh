a9fe877d4baf03b75398e90dc95066d01408e349
Matthew Gabeler-Lee <cheetah@fastcat.org>
2010-05-26 16:03:51 +0000

    fix for bsd tar

--- test/08-exclude.sh
+++ test/08-exclude.sh
@@ -7,7 +7,9 @@ set -xe
 [ -f bin/test/data.x.tar ]
 
 # using -eg means that 'data' won't match 'data.d'
-bin/tarix -zxf bin/test/data.tarix -t bin/test/data.tgz -eg data 'data.?/' 'data.?/data[2]' \
+# some tar implementations store directory names with a trailing /, others do not
+# so we need to exclude both in this mode
+bin/tarix -zxf bin/test/data.tarix -t bin/test/data.tgz -eg data 'data.?/' 'data.?' 'data.?/data[2]' \
   >bin/test/data.exclude.tar
 #hexdump -C bin/test/data.exclude.tar
 tar -tvf bin/test/data.exclude.tar
