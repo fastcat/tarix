5e147b4548d44c9ab656d92faf1c9e9637eef141
Matthew Gabeler-Lee <cheetah@fastcat.org>
2010-05-26 05:32:50 +0000

    remove excess "|| exit 1" items

--- tarix/test/04-index.sh
+++ tarix/test/04-index.sh
@@ -11,8 +11,8 @@ cp bin/test/data bin/test/data.d/data2
 sources="data data.d"
 
 tar -c -f - -C bin/test/ $sources | bin/tarix -zf bin/test/data.tarix >bin/test/data.tgz
-[ -f bin/test/data.tarix ] || exit 1
-[ -f bin/test/data.tgz ] || exit 1
+[ -f bin/test/data.tarix ]
+[ -f bin/test/data.tgz ]
 cat bin/test/data.tarix
 #zcat bin/test/data.tgz | hexdump -C
 tar -tzv -f bin/test/data.tgz
@@ -22,9 +22,9 @@ if tar --version | grep GNU.tar ; then
   tar -c -f - -C bin/test/ -V 'test backup of data, data.d/data*' \
     --listed-incremental=bin/test/data.list $sources \
     | bin/tarix -zf bin/test/data.incr.tarix >bin/test/data.incr.tgz
-  [ -f bin/test/data.list ] || exit 1
-  [ -f bin/test/data.incr.tarix ] || exit 1
-  [ -f bin/test/data.incr.tgz ] || exit 1
+  [ -f bin/test/data.list ]
+  [ -f bin/test/data.incr.tarix ]
+  [ -f bin/test/data.incr.tgz ]
   cat bin/test/data.incr.tarix
   #zcat bin/test/data.incr.tgz | hexdump -C
   tar -tzv -f bin/test/data.incr.tgz
--- tarix/test/05-extract.sh
+++ tarix/test/05-extract.sh
@@ -2,8 +2,8 @@
 
 set -xe
 
-[ -f bin/test/data.tgz ] || exit 1
-[ -f bin/test/data.tarix ] || exit 1
+[ -f bin/test/data.tgz ]
+[ -f bin/test/data.tarix ]
 
 bin/tarix -zxf bin/test/data.tarix -t bin/test/data.tgz data.d/data1 >bin/test/data.x.tar
 #hexdump -C bin/test/data.x.tar
@@ -11,8 +11,8 @@ tar -tvf bin/test/data.x.tar
 
 # bsd tar doesn't support incremental or volume label
 if tar --version | grep GNU.tar ; then
-  [ -f bin/test/data.incr.tgz ] || exit 1
-  [ -f bin/test/data.incr.tarix ] || exit 1
+  [ -f bin/test/data.incr.tgz ]
+  [ -f bin/test/data.incr.tarix ]
   bin/tarix -zxf bin/test/data.incr.tarix -t bin/test/data.incr.tgz data.d/data1 >bin/test/data.incr.x.tar
   #hexdump -C bin/test/data.incr.x.tar
   tar -tvf bin/test/data.incr.x.tar
